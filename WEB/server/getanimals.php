d320acf4e41ab413de3b96efdc28b59dad4e9d45
VladDuncea <vlad_duncea_31@yahoo.com>
2020-04-16 22:10:27 +0300

    getanimal intoarce date despre cereri animale
    Documentatia actualizata

--- WEB/server/getanimals.php
+++ WEB/server/getanimals.php
@@ -12,7 +12,30 @@ if($_POST['request_type'] == 0)
     else
         $sql="SELECT * FROM Pets WHERE pet_type = {$_POST['pet_type']} ORDER BY PID";
 
-    
+    //Get animals and format them
+    if(!$result = mysqli_query($conn,$sql))
+    {
+        $response["status"]=-1;  //Database error
+        error_log("Connection failed".mysqli_error($conn));   //Error logging
+        echo json_encode($response);
+        return;
+    }
+
+    $response['nr_animals'] = $result->num_rows;
+    $poz = 0;
+    while($row = mysqli_fetch_assoc($result))
+    {
+        $response['animals'][$poz]['PID'] = $row['PID'];
+        $response['animals'][$poz]['name'] = $row['pet_name'];
+        $response['animals'][$poz]['birthdate'] = date_diff(date_create("now") , date_create($row['pet_birthdate']))->y;
+        $response['animals'][$poz]['state'] = $row['pet_state'];
+        $response['animals'][$poz]['description'] = $row['pet_description'];
+        $response['animals'][$poz]['image'] = $row['pet_image'];
+        $response['animals'][$poz]['food'] = $row['pet_food'];
+        $response['animals'][$poz]['type'] = $row['pet_type'];
+        $response['animals'][$poz]['breed'] = $row['pet_breed'];
+        $poz++;
+    }
 }
 else if($_POST['request_type'] == 1)
 {
@@ -47,39 +70,54 @@ else if($_POST['request_type'] == 1)
     }
 
     //Get animals bound to user
-    $sql="SELECT * FROM Pets WHERE PID IN (SELECT PID FROM Users_Pets WHERE UID = $UID)";
+    $sql="  SELECT * FROM Pets
+            LEFT JOIN (SELECT PID,request_type,request_state FROM Requests WHERE UID = $UID) aux USING (PID) 
+            WHERE PID IN (SELECT PID FROM Users_Pets WHERE UID = $UID)";
 
-}
+    //Get animals and format them
+    if(!$result = mysqli_query($conn,$sql))
+    {
+        $response["status"]=-1;  //Database error
+        error_log("Connection failed".mysqli_error($conn));   //Error logging
+        echo json_encode($response);
+        return;
+    }
 
-//Get animals and format them
-if(!$result = mysqli_query($conn,$sql))
-{
-    $response["status"]=-1;  //Database error
-    error_log("Connection failed".mysqli_error($conn));   //Error logging
-    echo json_encode($response);
-    return;
-}
-
-$response['nr_animals'] = $result->num_rows;
-$poz = 0;
-while($row = mysqli_fetch_assoc($result))
-{
-    $response['animals'][$poz]['PID'] = $row['PID'];
-    $response['animals'][$poz]['name'] = $row['pet_name'];
-    $response['animals'][$poz]['birthdate'] = date_diff(date_create("now") , date_create($row['pet_birthdate']))->y;
-    $response['animals'][$poz]['state'] = $row['pet_state'];
-    $response['animals'][$poz]['description'] = $row['pet_description'];
-    $response['animals'][$poz]['image'] = $row['pet_image'];
-    $response['animals'][$poz]['food'] = $row['pet_food'];
-    $response['animals'][$poz]['type'] = $row['pet_type'];
-    $response['animals'][$poz]['breed'] = $row['pet_breed'];
-    $poz++;
+    $response['nr_animals'] = $result->num_rows;
+    $poz = 0;
+    while($row = mysqli_fetch_assoc($result))
+    {
+        $response['animals'][$poz]['PID'] = $row['PID'];
+        $response['animals'][$poz]['name'] = $row['pet_name'];
+        $response['animals'][$poz]['birthdate'] = date_diff(date_create("now") , date_create($row['pet_birthdate']))->y;
+        $response['animals'][$poz]['state'] = $row['pet_state'];
+        $response['animals'][$poz]['description'] = $row['pet_description'];
+        $response['animals'][$poz]['food'] = $row['pet_food'];
+        $response['animals'][$poz]['type'] = $row['pet_type'];
+        $response['animals'][$poz]['breed'] = $row['pet_breed'];
+        
+        if($row['request_type']!= null)
+        {
+            $response['animals'][$poz]['has_request'] = 1;
+            $response['animals'][$poz]['request_type'] = $row['request_type'];
+            $response['animals'][$poz]['request_type'] = $row['request_state'];
+        }
+        else
+        {
+            $response['animals'][$poz]['has_request'] = 0;
+        }
+        
+        $response['animals'][$poz]['image'] = $row['pet_image'];
+        $poz++;
+    }
 }
 
 
 
 //Data for entry_log
-session_start();
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
 if(isset($_SESSION['FirstName']))
     $user = $_SESSION['FirstName']." ".$_SESSION['LastName'];
 else 
