58e10e495583862d77436ccf3a737007ce7c1d8f
VladDuncea <vlad_duncea_31@yahoo.com>
2020-04-18 18:32:26 +0300

    Update Documentatie + fix sendemail

--- WEB/server/sendmail.php
+++ WEB/server/sendmail.php
@@ -1,25 +1,36 @@
 <?php
 //TRIMITERE EMAIL
-if(isset($_POST['subject'],$_POST['body'],$_POST['email']))
+if(!isset($_POST['security_code']) || $_POST['security_code']!= '8981ASDGHJ22123')
 {
-    //Send welcome email to new User
-    $subject = $_POST['subject'];
-    $txt = $_POST['body'];
-    $headers = "From: {$_POST['email']}" . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
-
-    if(mail("secondhome@fragmentedpixel.com",$subject,$txt,$headers)) //Email
+    if(isset($_POST['subject'],$_POST['body'],$_POST['email']))
     {
-        $response['status'] = 1;
+        //Send welcome email to new User
+        $subject = $_POST['subject'];
+        $txt = $_POST['body'];
+        $headers = "From: {$_POST['email']}" . "\r\n" .
+        'X-Mailer: PHP/' . phpversion();
+
+        if(mail("secondhome@fragmentedpixel.com",$subject,$txt,$headers)) //Email
+        {
+            $response['status'] = 1;
+        }
+        else
+        {
+            $response['status'] = -1;
+            $response['err_message'] = "Error on sending email";
+        }
+        entry_log("sendmail","Unknown",$response);     
+        echo json_encode($response);  
+        return;
     }
     else
     {
-        $response['status'] = -1;
-        $response['err_message'] = "Error on sending email";
+        $response["status"]=0;
+        $response["err_message"] = "Missing parameters";
+        entry_log("sendmail","Unknown",$response);     
+        echo json_encode($response);  
+        return;
     }
-    entry_log("sendmail","Unknown",$response);     
-    echo json_encode($response);  
-    return;
 }
 $response["status"]=-1;
 $response["err_message"] = "Unauthoried access";
