d3acadf98c7631618e24f7da70d8ae61c9034e1d
Pascu Bianca <bianca.pascu1999@yahoo.com>
2020-04-22 15:48:31 +0300

    adaugat pagina administrator pt adoptie si cazare

---
+++ WEB/admin_cazare.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <?php include 'pieces/head.php' ?>
+</head>
+
+<body class="hold-transition sidebar-mini" onload="get_animal(0,0);">
+    <!-- Site wrapper -->
+    <div class="wrapper">
+        
+        <!-- Navbar -->
+        <?php include 'pieces/navbar.php' ?>
+        <!-- /.navbar -->
+
+        <!-- Main Sidebar Container -->
+        <?php include 'pieces/sidebar.php' ?>
+        <!--/main sidebar-->
+
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+            <!-- Content Header (Page header) -->
+            <section class="content-header">
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                        <div class="col-sm-6">
+                            <h1>
+                            <i class="nav-icon fas fa-paw"></i> Cereri pentru cazare
+                            </h1>
+                        </div>
+                        <div class="col-sm-6">
+                            <ol class="breadcrumb float-sm-right">
+                                <li class="breadcrumb-item"><a href="index.php">Acasă</a></li>
+                                <li class="breadcrumb-item active"><i class="nav-icon fas fa-paw"></i> Cereri pentru cazare</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+                <!-- /.container-fluid -->
+            </section>
+
+            <!-- Main content -->
+            <section class="content">
+
+                <!-- Default box -->
+                <div class="card card-solid">
+                    <div class="card-body pb-0" >
+                        <div class="row d-flex justify-content-around" id='boxAnimals'>
+                        </div>
+                    </div>
+                </div>
+                <!-- /.card-body -->
+                <div class="card-footer">
+                    <nav aria-label="Contacts Page Navigation">
+                        <ul class="pagination justify-content-center m-0">
+                            <li class="page-item active"><a class="page-link" href="#">1</a></li>
+                            <li class="page-item"><a class="page-link" href="#">2</a></li>
+                            <li class="page-item"><a class="page-link" href="#">3</a></li>
+                            <li class="page-item"><a class="page-link" href="#">4</a></li>
+                            <li class="page-item"><a class="page-link" href="#">5</a></li>
+                            <li class="page-item"><a class="page-link" href="#">6</a></li>
+                            <li class="page-item"><a class="page-link" href="#">7</a></li>
+                            <li class="page-item"><a class="page-link" href="#">8</a></li>
+                        </ul>
+                    </nav>
+                </div>
+                <!-- /.card-footer -->
+        
+
+            </section>
+            <!-- /.content -->
+        </div>
+        <!-- /.content-wrapper -->
+
+        <!-- Footer-->
+        <?php include 'pieces/footer.php' ?>
+        <!-- /footer -->
+
+        <!-- Control Sidebar -->
+        <aside class="control-sidebar control-sidebar-dark">
+            <!-- Control sidebar content goes here -->
+        </aside>
+        <!-- /.control-sidebar -->
+    </div>
+    <!-- ./wrapper -->
+
+    <!-- jQuery -->
+    <script src="../plugins/jquery/jquery.min.js"></script>
+    <!-- Bootstrap 4 -->
+    <script src="../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="../dist/js/adminlte.min.js"></script>
+    <!-- AdminLTE for demo purposes -->
+    <script src="../dist/js/demo.js"></script>
+    <script src="../dist/js/getanimals.js"></script>
+
+    <!-- <style>
+  
+    .img-fluid {
+      max-width: 90%;
+      height: 80px;
+    }
+    </style> -->
+    
+    
+</body>
+
+</html>
