15f021d5dff125018d49e783188b67162a7ee86f
Pascu Bianca <bianca.pascu1999@yahoo.com>
2020-04-26 20:10:51 +0300

    accept/refuz cereri angajat

--- WEB/dist/js/animalrequest.js
+++ WEB/dist/js/animalrequest.js
@@ -1,27 +1,67 @@
 function optiuni_animal(request_type, PID) {
-    console.log(PID)
-    console.log(request_type)
+    // console.log(PID)
+    // console.log(request_type)
     $.post('http://secondhome.fragmentedpixel.com/server/animalrequest.php', { request_type: request_type, PID: PID }, function(data, status) {
 
-        console.log(`${status}`)
+        // console.log(`${status}`)
         if (`${status}` == 'success') {
             console.log("am ajuns aici")
             if (request_type == 1) {
                 var elem1 = document.getElementById(`${PID}a`);
                 document.getElementById(`${PID}c`).value = 'Asteapta cazarea'
                 document.getElementById(`${PID}c`).innerHTML = '<i class="fas fa-paw"></i> Asteapta cazarea'
+                document.getElementById(`${PID}c`).disabled = true;
                 elem1.style = "display: none"
             } else
             if (request_type == 0) {
                 var elem2 = document.getElementById(`${PID}c`);
                 elem2.style = "display: none"
                 var elem3 = document.getElementById(`${PID}a`)
-
+                document.getElementById(`${PID}a`).disabled = true;
                 elem3.value = 'Asteapta adoptia'
                 elem3.innerHTML = '<i class="fas fa-paw"></i> Asteapta adoptia'
-                console.log(elem3.value)
 
             }
         }
+    })
+}
+
+function optiuni_angajat(value, RID) {
+    console.log(RID)
+    console.log(value)
+    $.post('http://secondhome.fragmentedpixel.com/server/updaterequest.php', { value: value, RID: RID }, function(data, status) {
+        var json_data = JSON.parse(data)
+
+        if (json_data.status == 1) {
+            if (value == 1) {
+                var elem1 = document.getElementById(`${RID}radoptie`);
+                document.getElementById(`${RID}aadoptie`).value = 'Cerere acceptata'
+                document.getElementById(`${RID}aadoptie`).innerHTML = '<i class="fas fa-paw"></i> Cerere acceptata'
+                document.getElementById(`${RID}aadoptie`).disabled = true;
+                elem1.style = "display: none"
+            } else
+            if (value == -1) {
+                var elem2 = document.getElementById(`${RID}aadoptie`);
+                elem2.style = "display: none"
+                var elem3 = document.getElementById(`${RID}radoptie`)
+
+                elem3.value = 'Cerere adoptie refuzata'
+                elem3.innerHTML = '<i class="fas fa-paw"></i> Cerere refuzata'
+                document.getElementById(`${RID}radoptie`).disabled = true;
+            }
+
+
+        }
+        if (json_data.status == 0) {
+            console.log("greseala utilizator")
+        }
+        if (json_data.status == -1) {
+            console.log("eroare server")
+        }
+        if (json_data.request_updated == 1)
+            console.log("a fost actualizat")
+        if (json_data.request_updated == 0)
+            console.log("nu a fost actualizat")
+
     })
 }
--- WEB/dist/js/getanimals.js
+++ WEB/dist/js/getanimals.js
@@ -1,8 +1,10 @@
 function get_animal(request_type_active, pet_type) {
 
     $.post('http://secondhome.fragmentedpixel.com/server/getanimals.php', { request_type: request_type_active, pet_type: pet_type }, function(data, status) {
-        if (status == 1) {
-            console.log(typeof data)
+
+        var json_data = JSON.parse(data)
+        if (json_data.status == 1) {
+            // console.log(typeof data)
             var json_data = JSON.parse(data)
             nr_animals = json_data.nr_animals
             animals = json_data.animals
@@ -10,7 +12,7 @@ function get_animal(request_type_active, pet_type) {
             var type_animal = ["Toate", "Pisici", "Câini", "Rozătoare", "Reptile", "Păsări", "Acvatice"]
             for (var i = 0; i < nr_animals; i++) {
                 // col-sm-3
-                const card = `<div class="d-flex">
+                const card = `<div class="d-flex ">
                 <div class="card bg-light">
                     <div class="card-header text-muted border-bottom-0" >
                        ${type_animal[animals[i].type]}
@@ -60,14 +62,15 @@ function get_animal(request_type_active, pet_type) {
                     if (animals[i].has_request == 1) {
                         if (animals[i].request_type == 0) {
                             if (animals[i].request_state == 0) {
-                                buttonState2 = '<i class="fas fa-paw"></i> Asteaptă adopția';
+                                buttonState2 = '<i class="fas fa-paw"></i> Asteaptă darea spre adopție';
                             } else
                             if (animals[i].request_state == 1) {
-                                buttonState2 = '<i class="fas fa-paw"></i> Adoptat';
+                                buttonState2 = '<i class="fas fa-paw"></i> Cerere dare spre adopție acceptată';
                             } else
                             if (animals[i].request_state == -1) {
                                 // TODO cerere respinsa
-                                console.log("cerere respinsa")
+                                // console.log("cerere respinsa")
+                                buttonState1 = '<i class="fas fa-paw"></i> Cerere dare spre adopție respinsă';
                             }
                         } else
                         if (animals[i].request_type == 1) {
@@ -75,13 +78,12 @@ function get_animal(request_type_active, pet_type) {
                                 buttonState1 = '<i class="fas fa-paw"></i> Asteaptă cazarea';
                             } else
                             if (animals[i].request_state == 1) {
-                                if (animals[i].request_state == 0) {
-                                    buttonState1 = '<i class="fas fa-paw"></i> Cazat';
-                                }
+                                buttonState1 = '<i class="fas fa-paw"></i> Cerere cazare acceptată';
                             } else
                             if (animals[i].request_state == -1) {
                                 // TODO cerere respinsa
-                                console.log("cerere respinsa")
+                                // console.log("cerere respinsa")
+                                buttonState1 = '<i class="fas fa-paw"></i> Cerere cazare respinsă';
                             }
                         } else
                         if (animals[i].request_type == 2) {
@@ -143,34 +145,14 @@ function get_animal(request_type_active, pet_type) {
                     // console.log(`${buttonState1}`)
                     // console.log(`${buttonState2}`)
                     // console.log("-----------------------")
-                } else if (request_type_active == 2) {
+                } else if (request_type_active == 2 || request_type_active == 3) {
 
                     const animal_box = card + `
                 <div class="card-body" style="padding: 2">   
                 <p class="lead" ><b>Utilizator: </b> <a href='#' >${animals[i].first_name} ${animals[i].last_name} </a></p>       
                     <div class="text-center">
-                    <button type="button" class="btn btn-secondary" id='${animals[i].PID}acazare' ><i class="fas fa-paw"id='detalii'></i> Accepta</button>    
-                    <button type="button" class="btn btn-secondary" id='${animals[i].PID}rcazare' ><i class="fas fa-paw"id='detalii'></i> Refuza</button> 
-                    </div> 
-                </div>
-                </div>
-             </div>`;
-                    // console.log(document.getElementById('boxAnimals'));
-
-                    if (document.getElementById('boxAnimals') != null) {
-                        document.getElementById('boxAnimals').innerHTML += animal_box;
-                    } else {
-                        console.log('Nu s-a gasit animale.php')
-                    }
-
-                } else if (request_type_active == 3) {
-
-                    const animal_box = card + `
-                <div class="card-body" style="padding: 2">    
-                <p class="lead" ><b>Utilizator: </b> <a href='#' >${animals[i].first_name} ${animals[i].last_name} </a></p>       
-                    <div class="text-center">
-                    <button type="button" class="btn btn-secondary" id='${animals[i].PID}acazare' ><i class="fas fa-paw"id='detalii'></i> Accepta</button>    
-                    <button type="button" class="btn btn-secondary" id='${animals[i].PID}rcazare' ><i class="fas fa-paw"id='detalii'></i> Refuza</button> 
+                    <button type="button" class="btn btn-secondary" id='${animals[i].RID}aadoptie' onclick="optiuni_angajat(1,${animals[i].RID});"><i class="fas fa-paw"id='detalii'></i> Accepta</button>    
+                    <button type="button" class="btn btn-secondary" id='${animals[i].RID}radoptie' onclick="optiuni_angajat(-1,${animals[i].RID});"><i class="fas fa-paw"id='detalii'></i> Refuza</button> 
                     </div> 
                 </div>
                 </div>
