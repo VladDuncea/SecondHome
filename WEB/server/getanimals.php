26f9f8229afa39585e4b06f54ec540c7ef324742
VladDuncea <vlad_duncea_31@yahoo.com>
2020-06-04 21:16:35 +0300

    Update getanimals.php
    Implemented feedback for  rehoming

--- WEB/server/getanimals.php
+++ WEB/server/getanimals.php
@@ -2,6 +2,7 @@
 include "php/connection.php";
 include "php/header.php";
 $response["status"]=-1;
+$response["hasAccount"] = 0;
 
 //GET REQUEST TYPE
 if(!isset($_POST['request_type']))
@@ -14,18 +15,39 @@ if(!isset($_POST['request_type']))
 }
 
 $req_type = $_POST['request_type'];
+
+
+//get user data
+getUserData($UID,$UType,"getanimals",$conn);
+if($UID == -2)
+{
+    return;
+}
+
+
 //0 - get all animals waiting for addoption
 if($req_type == 0)
 {
     //type of animals to be returned
-    if(!isset($_POST['pet_type']) || $_POST['pet_type']==0)
-        $sql="SELECT * FROM Pets WHERE pet_state=10 ORDER BY PID";
+    if($UID != -1)
+    {
+        $sql="SELECT * FROM Pets 
+                LEFT JOIN (SELECT PID,request_state 
+                            FROM Requests 
+                            WHERE UID = $UID AND request_type = 2) aux USING (PID) WHERE ";
+    }
     else
     {
-        $pet_type = mysqli_real_escape_string( $conn,$_POST['pet_type']);
-        $sql="SELECT * FROM Pets WHERE pet_type = $pet_type AND pet_state=10 ORDER BY PID";
+        $sql="SELECT * FROM Pets WHERE ";
     }
-        
+    
+    if(isset($_POST['pet_type']) && $_POST['pet_type']!=0)
+    {
+        $pet_type = mysqli_real_escape_string( $conn,$_POST['pet_type']);
+        $sql .= "pet_type = $pet_type AND ";
+    }   
+    $sql .="pet_state=10 ORDER BY PID";
+    
 
     //Get animals and format them
     if(!$result = mysqli_query($conn,$sql))
@@ -47,50 +69,77 @@ if($req_type == 0)
         $response['animals'][$poz]['image'] = $row['pet_image'];
         $response['animals'][$poz]['type'] = $row['pet_type'];
         $response['animals'][$poz]['breed'] = $row['pet_breed'];
+        if($UID!=-1)
+        {
+            if($row['request_state']!= null)
+            {
+                $response['animals'][$poz]['has_request'] = 1;
+                $response['animals'][$poz]['request_state'] = $row['request_state'];
+            }
+            else
+            {
+                $response['animals'][$poz]['has_request'] = 0;
+            }
+        }
+        
         $poz++;
     }
 }   //ELEVATED ACCES FUNCTIONS(MY PETS + GET REQUEST PETS)
-else if($req_type >= 1 && $req_type <= 3)
+else if($req_type >= 1 && $req_type <= 5)
 {
-    //GENERAL UID/UType getter
-    if(!isset($_POST['security_code']))
-    {
-        //WEB
-        //Verify account level
-        session_start();
-        if(!isset($_SESSION['userType']))
-        {
-            $response["status"]=-1;
-            $response["err_message"] = "Unauthoried access";
-            entry_log("getanimals","Unknown",$response);     
-            echo json_encode($response);  
-            return;
-        }
-        $UID = $_SESSION['userId'];
-        $UType = $_SESSION['userType'];
-        
-    }
-    else if($_POST['security_code'] == '8981ASDGHJ22123' && isset($_POST['UID']))
-    {
-        //ANDROID
-        $UID = mysqli_real_escape_string( $conn,$_POST['UID']);
-        $sql = "SELECT user_type FROM Users WHERE UID = $UID";
-        if(!$result = mysqli_query($conn,$sql))
-        {
-            $response["status"]=-1;  //Database error
-            error_log("SQL ERROR: ".mysqli_error($conn));   //Error logging
-            echo json_encode($response);
-            return;
-        }
-        $UType = mysqli_fetch_assoc($result)['user_type'];
-    }
-    else
+    if($UID == -1)
     {
         $response["status"]=0;
-        $response["err_message"] = "Missing parameters!";
+        $response["err_message"] = "Unauthorized access";
         entry_log("getanimals","Unknown",$response);     
         echo json_encode($response);  
         return;
+    }
+
+    //PETS ADOPTED BY CERTAIN USER
+    if($req_type == 5)
+    {
+        //Get animals bound to user
+        $sql="  SELECT * FROM Pets
+        LEFT JOIN (SELECT PID,request_type,request_state FROM Requests WHERE UID = $UID) aux USING (PID) 
+        WHERE PID IN (SELECT PID FROM Users_Pets WHERE UID = $UID)
+            AND pet_state = 11
+        ORDER BY PID DESC";
+
+        //Get animals and format them
+        if(!$result = mysqli_query($conn,$sql))
+        {
+            $response["status"]=-1;  //Database error
+            error_log("Connection failed".mysqli_error($conn));   //Error logging
+            echo json_encode($response);
+            return;
+        }
+
+        $response['nr_animals'] = $result->num_rows;
+        $poz = 0;
+        while($row = mysqli_fetch_assoc($result))
+        {
+            $response['animals'][$poz]['PID'] = $row['PID'];
+            $response['animals'][$poz]['name'] = $row['pet_name'];
+            $response['animals'][$poz]['birthdate'] = date_diff(date_create("now") , date_create($row['pet_birthdate']))->y;
+            $response['animals'][$poz]['description'] = $row['pet_description'];
+            $response['animals'][$poz]['type'] = $row['pet_type'];
+            $response['animals'][$poz]['breed'] = $row['pet_breed'];
+
+            if($row['request_type']!= null)
+            {
+                $response['animals'][$poz]['has_request'] = 1;
+                $response['animals'][$poz]['request_type'] = $row['request_type'];
+                $response['animals'][$poz]['request_state'] = $row['request_state'];
+            }
+            else
+            {
+                $response['animals'][$poz]['has_request'] = 0;
+            }
+
+            $response['animals'][$poz]['image'] = $row['pet_image'];
+            $poz++;
+        }
     }
 
     //PETS OF CERTAIN USER
@@ -100,6 +149,7 @@ else if($req_type >= 1 && $req_type <= 3)
         $sql="  SELECT * FROM Pets
         LEFT JOIN (SELECT PID,request_type,request_state FROM Requests WHERE UID = $UID) aux USING (PID) 
         WHERE PID IN (SELECT PID FROM Users_Pets WHERE UID = $UID)
+        AND pet_state <> 11
         ORDER BY PID DESC";
 
         //Get animals and format them
@@ -199,6 +249,13 @@ if(isset($_SESSION['FirstName']))
     $user = $_SESSION['FirstName']." ".$_SESSION['LastName'];
 else 
     $user = 'Guest';
+
+if($UID != -1)
+{
+    $response["hasAccount"] = 1;
+}
+
+    
 
 //No error
 $response["status"] = 1; 
